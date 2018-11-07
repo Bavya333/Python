aba79eb2a049419e2e032552dd92b1769003ae2c
Bavya333 <vostrogeek@gmail.com>
2018-11-07 18:02:00 +0530

    Create default_arg.py
    This function should print the first  values returned by get_next() method of stream object provided as an argument. Each of these values should be printed in a separate line.
    Whenever the function is called without the stream argument, it should use an instance of EvenStream class defined in the code stubs below as the value of stream.

---
+++ default_arg.py
@@ -0,0 +1,36 @@
+class EvenStream(object):
+    def __init__(self):
+        self.current = 0
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+
+class OddStream(object):
+    def __init__(self):
+        self.current = 1
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+
+def print_from_stream(n, stream=None):
+    if stream is None:
+        s=EvenStream()
+        for i in range(n):
+            print s.get_next()
+    else:
+        s=OddStream()
+        for i in range(n):
+            print s.get_next()
+
+queries = int(raw_input())
+for _ in range(queries):
+    stream_name, n = raw_input().split()
+    n = int(n)
+    if stream_name == "even":
+        print_from_stream(n)
+    else:
+        print_from_stream(n, OddStream())
